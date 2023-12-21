5c651164c2230e121473fb7ebc4c6240162816ff
Wiktor <w.zawadzki@mail.com>
2023-12-21 19:28:42 +0100

    code update

--- MyLibraries/Tests/card_game_war/player_class.py
+++ MyLibraries/Tests/card_game_war/player_class.py
@@ -6,20 +6,15 @@ class PlayerHand(SingleCard):
 
     '''*************************************************************************************
     @name       __init__
-    @brief      ...
-
-    @param[in]  void
-
+    @brief      Initialization of a Player class object
+    @param[in]  playerName
     @note       ...
     @return     None
     '''
-    def __init__(self, inCardList= None):
-        if type(inCardList) == list:
-            self.__INTERN_handList = inCardList
-            self.__INTERN_cardsAmount = len(self.handList)
-        else:
-            self.__INTERN_handList = []
-            self.__INTERN_cardsAmount = 0x00
+    def __init__(self, playerName):
+        self.playerName = playerName
+        self.__INTERN_handList = []
+        self.__INTERN_cardsAmount = 0x00
         return None
     #=======================================================================================
 
@@ -30,7 +25,7 @@ class PlayerHand(SingleCard):
     @note       ...
     @return     ...
     '''
-    def RemoveCard(self):
+    def RemoveLastCard(self):
         if self.__INTERN_cardsAmount > 0x00:
             self.__INTERN_cardsAmount = self.__INTERN_cardsAmount - 0x01
             return self.__INTERN_handList.pop()
@@ -45,12 +40,40 @@ class PlayerHand(SingleCard):
     @note       ...
     @return     ...
     '''
-    def AddCard(self, inCard):
-        if type(inCard) == SingleCard:
-            self.__INTERN_handList.append(inCard)
+    def AddCard(self, singleCardObj):
+        if type(singleCardObj) == SingleCard:
+            self.__INTERN_handList.append(singleCardObj)
             self.__INTERN_cardsAmount = self.__INTERN_cardsAmount + 0x01
             return True
         else:
             return False
+    #=======================================================================================
+    
+    '''*************************************************************************************
+    @name       GetCardStr
+    @brief      Retun string from class element
+    @param[in]  indexCard - Index of a card element class
+    @note       str( method __str__ )
+    @return     string 
+    '''
+    def GetCardStr(self, indexCard):
+        if type(indexCard) == int:
+            if indexCard < self.__INTERN_cardsAmount:
+                return str(self.__INTERN_handList[indexCard])
+        return "EMPTY"
+    #=======================================================================================
+        
+    '''*************************************************************************************
+    @name       GetCardValue
+    @brief      Return value from class element
+    @param[in]  indexCard - Index of a card element class
+    @note       int( method __int__ )
+    @return     int value of a card
+    '''
+    def GetCardValue(self, indexCard):
+        if type(indexCard) == int:
+            if indexCard < self.__INTERN_cardsAmount:
+                return int(self.__INTERN_handList[indexCard])
+        return 0x00
     #=======================================================================================
 #===========================================================================================
--- MyLibraries/Tests/testMyFunctions.py
+++ MyLibraries/Tests/testMyFunctions.py
@@ -1,5 +1,6 @@
 #from card_game_war.card_class import *
 from card_game_war.deck_class import *
+from card_game_war.player_class import *
 
 def CALL_test_card():
     instDeckCards = DeckCards(0x01)
@@ -11,6 +12,10 @@ def CALL_test_card():
             print("- {} - left {}".format(hex(cardNum), instDeckCards.index))
         else:
             break
+
+def CALL_test_player():
+    instPlayer = PlayerHand("Player1")
+    instPlayer.AddCard(Card)
 
 if __name__ == "__main__":
     CALL_test_card()
